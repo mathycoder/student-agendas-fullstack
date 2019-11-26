437091469f7ae315b86f210d6eea9a556c3195d9
Adam Sarli <adam.sarli@gmail.com>
2019-11-26 12:01:54 -0500

    added textarea to Reflection

--- client/src/components/reflections/NewReflection.js
+++ client/src/components/reflections/NewReflection.js
@@ -9,6 +9,10 @@ class NewReflection extends Component {
           <div className="lined-paper">
             <div className="lined-paper-form">
               <div className="paper-form-title">Create a Reflection Question</div>
+              <button className="add-prog-button">Add to Progression</button>
+              <div className="reflection-question">
+                <textarea placeholder="Enter your question here"></textarea>
+              </div>
             </div>
           </div>
         </div>
