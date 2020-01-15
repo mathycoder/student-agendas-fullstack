907f7f0109d9fa01f48f46712ce070eb676dfd10
Adam Sarli <adam.sarli@gmail.com>
2020-01-15 08:55:27 -0500

    another slight variation of render file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::API
 
   def fallback_index_html
     # render :file => 'index.html'
-    render file: "#{Rails.root}/public/index.html", layout: false
+    #render file: "#{Rails.root}/public/index.html", layout: false
+    render :file => 'public/index.html'
   end
 
   def current_user
