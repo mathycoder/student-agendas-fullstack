c5cb5a5b1120e7681777c2888282d930ea8947fa
Adam Sarli <adam.sarli@gmail.com>
2020-01-19 20:03:27 -0500

    placeholder fixed glitches

--- client/src/components/students/StudentAgenda.js
+++ client/src/components/students/StudentAgenda.js
@@ -2,50 +2,48 @@ import React from 'react'
 import StudentAgendaProgression from './StudentAgendaProgression'
 import { Droppable, Draggable } from 'react-beautiful-dnd'
 
-const StudentAgenda = ({ innerRef, handleDeleteProgClick, progressions, student }) => {
+const StudentAgenda = ({ handleDeleteProgClick, progressions, student }) => {
   return (
+    <div className={`student-agenda`}
+      data-student-id={student.id}
+      >
+        <div className="undroppable-area">
+          {progressions.filter(pr => pr.submitted).map((progression, index) => {
+            return (
+              <div key={index}>
+                <StudentAgendaProgression
+                  progression={progression}/>
+              </div>
+          )})}
+        </div>
 
-
-        <div className={`student-agenda`}
-          data-student-id={student.id}
-          >
-            <div className="undroppable-area">
-              {progressions.filter(pr => pr.submitted).map((progression, index) => {
+        <Droppable droppableId={`droppable-student${student.id}`} direction="horizontal">
+          {(provided2) => (
+            <div
+              ref={node => provided2.innerRef(node)}
+              {...provided2.droppableProps}
+              className="droppable-area">
+              {progressions.filter(pr => !pr.submitted).map((progression, index) => {
                 return (
-                  <div key={index}>
-                    <StudentAgendaProgression
-                      progression={progression}/>
-                  </div>
+                  <Draggable draggableId={`student${student.id}-progression${progression.id}`} index={index} key={`student-${student.id}-progression-${progression.id}-${index}`}>
+                    {(provided) => (
+                      <div {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <StudentAgendaProgression
+                          innerRef={provided.innerRef}
+                          key={index}
+                          placedholder={provided.placeholder}
+                          handleDeleteProgClick={handleDeleteProgClick}
+                          progression={progression}/>
+                      </div>
+                    )}
+                  </Draggable>
               )})}
+              {provided2.placeholder}
             </div>
 
-            <Droppable droppableId={`droppable-student${student.id}`} direction="horizontal">
-              {(provided2) => (
-                <div
-                  ref={node => provided2.innerRef(node)}
-                  {...provided2.droppableProps}
-                  className="droppable-area">
-                  {progressions.filter(pr => !pr.submitted).map((progression, index) => {
-                    return (
-                      <Draggable draggableId={`student${student.id}-progression${progression.id}`} index={index} key={`student-${student.id}-progression-${progression.id}-${index}`}>
-                        {(provided) => (
-                          <div {...provided.draggableProps} {...provided.dragHandleProps}>
-                            <StudentAgendaProgression
-                              innerRef={provided.innerRef}
-                              key={index}
-                              placedholder={provided.placeholder}
-                              handleDeleteProgClick={handleDeleteProgClick}
-                              progression={progression}/>
-                          </div>
-                        )}
-                      </Draggable>
-                  )})}
-                </div>
-
-              )}
-            </Droppable>
-          </div>
-
+          )}
+        </Droppable>
+      </div>
   )
 }
 
