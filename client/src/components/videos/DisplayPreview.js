159d1c5c0a52deb128aa5ce4296a3e4a35791642
Adam Sarli <adam.sarli@gmail.com>
2020-01-20 19:43:50 -0500

    <DisplayPreview /> uses flexbox

--- client/src/components/videos/DisplayPreview.js
+++ client/src/components/videos/DisplayPreview.js
@@ -4,24 +4,24 @@ import './VideoSearch.css';
 const DisplayPreview = ({video, addToProgression, removeFromProgression, shiftup}) => {
   return (
     <div className={shiftup ? "shiftup search-video-preview" : "search-video-preview" } >
-      <h4>{video.title}</h4><br />
-      <h6>
-        Created by {video.channelTitle} on {video.date}
-      </h6>
-      <br/>
-      <div>
+      <div className="title-and-video">
+        <h4>{video.title}</h4><br />
+        <h6>Created by {video.channelTitle} on {video.date}</h6>
+        <br/>
+        <iframe
+          title="video preview"
+          id="video" type="text/html"
+          src={video.url}
+          frameBorder="0"
+          allowFullScreen
+          >
+        </iframe>
       </div>
-      <iframe
-        title="video preview"
-        id="video" type="text/html"
-        src={video.url}
-        frameBorder="0"
-        allowFullScreen
-        >
-      </iframe>
-      <p>{video.description}</p>
-      {addToProgression ? <button onClick={(event) => addToProgression(event, video)}>Add to Progression</button> : ''}
-      {removeFromProgression ? <button onClick={(event) => removeFromProgression(video)}>Remove from Progression</button> : ''}
+      <div className="description-and-button">
+        <p>{video.description}</p>
+        {addToProgression ? <button onClick={(event) => addToProgression(event, video)}>Add to Progression</button> : ''}
+        {removeFromProgression ? <button onClick={(event) => removeFromProgression(video)}>Remove from Progression</button> : ''}
+      </div>
     </div>
   )
 }
