1c454fd2e133bdf7adbfcfe61250fe4efda5d0b2
Adam Sarli <adam.sarli@gmail.com>
2020-01-22 12:07:54 -0500

    added validation for Student :username; Student#generate login iterates through possible usernams until it finds a unique one

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,12 +7,23 @@ class Student < ApplicationRecord
   has_many :reflections, through: :items
 
   validates :firstName, presence: true, length: { maximum: 12, minimum: 3 }
-  validates :firstName, uniqueness: { scope: :lastName, message: "A student with that first and last name already exists in the database" }
+  #validates :firstName, uniqueness: { scope: :lastName, message: "A student with that first and last name already exists in the database" }
+  validates :username, uniqueness: true, presence: true
+
   validates :lastName, presence: true, length: { maximum: 12, minimum: 3 }
 
 
   def generate_login
-    self.username = self.firstName[0].downcase + self.lastName.downcase if self.firstName.length > 0 && self.lastName.length > 0
+    base_name = self.firstName[0].downcase + self.lastName.downcase if self.firstName.length > 0 && self.lastName.length > 0
+    username = base_name
+
+    counter = 1
+    while Student.find_by(username: username)
+      username = "#{base_name}#{counter}"
+      counter += 1
+    end
+
+    self.username = username
     self.password = (0...6).map { ('a'..'z').to_a[rand(26)] }.join
   end
 end
